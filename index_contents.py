f2cd35d04b3d5b95c3a181d7a88f15844f03aad7
ayobi <17304717+ayobi@users.noreply.github.com>
2023-05-19 17:29:43 -0400

    read description from yml

--- index_contents.py
+++ index_contents.py
@@ -1,32 +1,31 @@
 import glob
 import json
 import os
+import yaml
 
 
-def readme(f):
-	readme_pth = os.path.join(*f_split[:-1], "README_" + f_split[-1] + ".txt")
-	if os.path.isfile(readme_pth):
-		with open(readme_pth, "r") as r:
-			content = r.read()
-		return content
-	else:
-		return None
+def read_yaml_description(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        description = data.get('description')
+        return description
 
 
 files_list = glob.glob("**/*.yaml", recursive=True)
 
 tpls = {}
 for f in files_list:
-	# print(f)
-	f_split = f.split("/")
-	basename, ext = os.path.splitext(f)
-	f_split = basename.split("/")
-	f_split
-	tpl_name = f_split[-1]
-	tpls[tpl_name] = {
-		"url": f,
-		"description": readme(f)
-	}
+    # print(f)
+    f_split = f.split("/")
+    basename, ext = os.path.splitext(f)
+    f_split = basename.split("/")
+    f_split
+    tpl_name = f_split[-1]
+    description = read_yaml_description(f)
+    tpls[tpl_name] = {
+        "url": f,
+        "description": description
+    }
 
 with open("list.json", "w") as outfile:
-	json.dump(tpls, outfile, indent=4)
+    json.dump(tpls, outfile, indent=4)
