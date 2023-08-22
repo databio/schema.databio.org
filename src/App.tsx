395e29d3d5590b2b669f856e2c3f4701222ec134
ayobi <17304717+ayobi@users.noreply.github.com>
2023-08-22 11:01:06 -0400

    react upgrade fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,48 @@
 import './App.css'
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Bulker from './pages/bulker';
-import Pep from './pages/pep';
-import Pipelines from './pages/pipelines';
-import Refgenie from './pages/refgenie';
-import Refget from './pages/refget';
-
+import Select from 'react-select';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  
+  interface SchemaOption {
+    value: string;
+    label: string;
+  }
+  
+  const schemaOptions: SchemaOption[] = [
+    { value: '/bulker/', label: 'Bulker' },
+    { value: '/pep/', label: 'Pep' },
+    { value: '/pipelines/', label: 'Pipelines' },
+    { value: '/refgenie/', label: 'Refgenie' },
+    { value: '/refget/', label: 'Refget' },
+  ];
+
+  const navigate = useNavigate();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = (selectedOption: any) => {
+    navigate(selectedOption.value)
+  };
 
   return (
     <>
-      <div className="container">
-        <div className="header">
-          <h1>Lab schemas</h1>
+      <div>
+        <div>
+          <h1>Databio schemas</h1>
         </div>
         <h2>About</h2>
 
         Here you'll find an API for databio lab schemas like bulker, pep, pipelines, refgenie, refget, etc.
 
-        <h2>Contributing a schema</h2>
+        <h2>Search schema categories below</h2>
 
-        <h4>Write a schema</h4>
-
-        <p>First, you have to write a schema yaml file. It should follow json-schema format.</p>
-
-        <h4>Upload your schema</h4>
-
-        <p>After creating your schema file, you can contribute it to this repository so that you and others can more easily load it. Name your schema yaml file with the name of the schema. Schemas in the registry are divided into namespaces, which are represented as subfolders in this repository. So, place your schema into an appropriate subfolder, and then open a pull request.</p>
-
-        <h2>Below is a list of schema categories</h2>
+        <Select
+          options={schemaOptions}
+          onChange={handleChange}
+        />
       </div>
-      <Tabs
-        defaultActiveKey="bulker"
-        id="uncontrolled-tab-example"
-        className="mb-3"
-      >
-        <Tab eventKey="bulker" title="Bulker">
-          <Bulker />
-        </Tab>
-        <Tab eventKey="pep" title="PEP">
-          <Pep />
-        </Tab>
-        <Tab eventKey="pipelines" title="Pipelines">
-          <Pipelines />
-        </Tab>
-        <Tab eventKey="refgenie" title="Refgenie">
-          <Refgenie />
-        </Tab>
-        <Tab eventKey="refget" title="Refget">
-          <Refget />
-        </Tab>
-      </Tabs>
-      
     </>
   )
 }
 
 export default App
-
-
-
---
+++ src/routes/contribute.tsx
@@ -0,0 +1,26 @@
+
+const Contribute = () => {
+
+
+  return (
+    <>
+        <div>
+        <div>
+            <h1>Contributing a schema</h1>
+        </div>
+        <h2>Write a schema</h2>
+
+        <p>First, you have to write a schema yaml file. It should follow json-schema format.</p>
+
+        <h2>Upload your schema</h2>
+
+        <p>After creating your schema file, you can contribute it to this repository so that you and others can more easily load it. Name your schema yaml file with the name of the schema. Schemas in the registry are divided into namespaces, which are represented as subfolders in this repository. So, place your schema into an appropriate subfolder, and then open a pull request.</p>
+
+
+        
+        </div>
+    </>
+  )
+}
+
+export default Contribute
