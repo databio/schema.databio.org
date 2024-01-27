1fbdc7775977420c421c95db52344de3c580b9b1
nsheff <nsheff@users.noreply.github.com>
2024-01-27 14:26:18 -0500

    fix link hover icon

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ const NamespaceDashboard = (props) => {
 const SchemaList = (props) => {
   const listItems = Object.values(props.schemas).map(schema => { 
     return <li key={schema.schema}>
-      <a className="a" onClick={() => props.selectSchema(schema.schema)}>{schema.schema}</a> -- {schema.description}  <a href={schema.url}>Download</a>
+      <a href={`/?namespace=${schema.namespace}&schema=${schema.schema}`} className="a" onClick={(e) => { e.preventDefault(); props.selectSchema(schema.schema); } }>{schema.schema}</a> -- {schema.description}  <a href={schema.url}>Download</a>
     </li>
   })
   console.log(props.schemas)
@@ -144,7 +144,36 @@ const SchemaSelector = (props) => {
     return (
       <>
         <h2>Select namespace</h2>
+        
         <Select options={namespaceList} onChange={selectNamespace} />
+
+        <hr/>
+
+        <h2>Guide</h2>
+        This is a schema registry and API. It is hosted on GitHub Pages. 
+        Schemas are identified by a <i>namespace</i> and <i>schema name</i>, put together to form a *registry path*, of this form:
+        
+        <br/><br/>
+
+        <pre><code>&#123;namespace&#125;/&#123;schema&#125;</code></pre>
+        
+        <h3>Browsing</h3>
+        
+        To browse existing schemas, select a namespace from the dropdown above. This will show you a list of all schemas in that namespace
+
+        <h3>API</h3>
+
+        To access a schema via the API, use the following URL format:
+        <br/><br/>
+        <pre><code>https://schema.databio.org/&#123;namespace&#125;/&#123;schema&#125;.yaml</code></pre>
+
+        You can also get a list of schemas in a namespace via the API:
+
+        <br/><br/>
+
+        <pre><code>https://schema.databio.org/&#123;namespace&#125;/schemas.json</code></pre>
+
+        <h3>Contributing</h3>You can contribute your own schema via PR to the GitHub repository.
       </>
     );
 }
