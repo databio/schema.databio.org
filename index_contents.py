9b285154e914d7abac0ee8b95136a9efa49fa7c4
nsheff <nsheff@users.noreply.github.com>
2024-01-27 08:07:55 -0500

    cleanup

--- index_contents.py
+++ index_contents.py
@@ -1,33 +1,67 @@
 import glob
 import json
 import os
+import shutil
 import yaml
 
-
 def read_yaml_description(file_path):
+    """
+    Extract the schema description from the yaml file
+    """
     with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
         description = data.get('description')
         return description
 
 
-files_list = glob.glob("**/*.yaml", recursive=True)
+# List of all schemas
+files_list = glob.glob("schemas/**/*.yaml", recursive=True)
 
 tpls = {}
+schema_metadata = {}
 for f in files_list:
-    # print(f)
+    print(f)
     f_split = f.split("/")
     basename, ext = os.path.splitext(f)
     f_split = basename.split("/")
     f_split
-    namespace = f_split[0]
+    namespace = f_split[1]
     tpl_name = f_split[-1]
     description = read_yaml_description(f)
-    tpls[basename] = {
-        "project": namespace,
-        "url": f,
+    url = os.path.relpath(f, "schemas/")
+    if namespace not in schema_metadata:
+        schema_metadata[namespace] = {}
+    
+    schema_metadata[namespace][tpl_name] = {
+        "namespace": namespace,
+        "schema": f_split[-1],
+        "url": url,
         "description": description
     }
 
-with open("list.json", "w") as outfile:
+    tpls[basename] = {
+        "project": namespace,
+        "url": url,
+        "description": description
+    }
+    new_file = f"public/{url}"
+    os.makedirs(os.path.dirname(new_file), exist_ok=True)
+    shutil.copyfile(f, new_file)
+
+
+print(json.dumps(schema_metadata, indent=4))
+print("Namespaces: ", list(schema_metadata.keys()))
+
+# Namespaces list
+with open("public/namespaces.json", "w") as outfile:
+    json.dump(list(schema_metadata.keys()), outfile, indent=4)
+
+# Schema list for each namespace
+for namespace in schema_metadata:
+    with open(f"public/{namespace}/schemas.json", "w") as outfile:
+        json.dump(schema_metadata[namespace], outfile, indent=4)
+
+# List of all schemas
+with open("public/list.json", "w") as outfile:
     json.dump(tpls, outfile, indent=4)
+
---
+++ src/App.jsx
@@ -0,0 +1,152 @@
+import { useSearchParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import Select from 'react-select'
+
+const App = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+    const [schema, setSchema] = useState();
+    const [namespace, setNamespace] = useState();
+    useEffect(() => {
+      setSchema(searchParams.get('schema'))
+      setNamespace(searchParams.get('namespace'))
+    })
+
+
+    return (
+        <div>
+        {/* Breadcrumbs */}
+        <a href="/">Home</a>  {namespace ? <> &#187; <a href={`/?namespace=${namespace}`}>{namespace}</a> </> : "" } &#187; {schema ? <>{schema}</> : "..."}
+
+        <ViewSwitcher schema={schema} namespace={namespace} setNamespace={setNamespace} setSchema={setSchema} setSearchParams={setSearchParams}/>
+        </div>
+    );
+};
+
+const ViewSwitcher = (props) => {
+    if (props.namespace) {
+        if (props.schema) {
+            return <SchemaDashboard namespace={props.namespace} schema={props.schema}/>
+        } else {
+            return <NamespaceDashboard namespace={props.namespace} setSchema={props.setSchema} setSearchParams={props.setSearchParams} />
+        }               
+    } else {
+        return <SchemaSelector setNamespace={props.setNamespace} setSearchParams={props.setSearchParams}/>
+    }
+}
+
+const SchemaDashboard = (props) => {
+  const [schema, setSchema] = useState([]);
+  useEffect(() => {
+    fetch(`/${props.namespace}/${props.schema}.yaml`)
+      .then(response => response.text())
+      .then(data => {
+        setSchema(data)
+        console.log("schema:", data)
+      })
+  }, [])
+
+  const divStyle = {
+    'backgroundColor': '#eee',
+    'padding': '10px',
+    'fontSize': '0.7em',
+  }
+
+    return (
+        <div>
+        <h3>Schema</h3>
+        <b>Description:</b> {schema.description}<br/>
+        <b>Relative path:</b> {`${props.namespace}/${props.schema}.yaml`} <br/>
+        <b>Endpoint:</b> https://schema.databio.org/{`${props.namespace}/${props.schema}.yaml`}<br/>
+        <b>Preview (this page):</b> https://schema.databio.org/?namespace={`${props.namespace}`}&schema={`${props.schema}`}<br/>
+
+        <button className="btn btn-primary my-4" href={`${props.namespace}/${props.schema}.yaml`}>Download schema</button>
+
+        <h4>Preview</h4>
+        <pre style={divStyle}><code>{schema}</code></pre>
+        </div>
+    );
+}
+
+
+const NamespaceDashboard = (props) => {
+  const [schemaList, setSchemaList] = useState([]);
+  useEffect(() => {
+    fetch(`/${props.namespace}/schemas.json`)
+      .then(response => response.json())
+      .then(data => {
+        setSchemaList(data)
+        console.log("schemaList:", Object.values(data))
+      })
+  }, [])
+
+  const selectSchema = (schema) => {
+
+    props.setSearchParams((prevParams) => { 
+      prevParams.append('schema', schema)
+      return prevParams
+    })
+    props.setSchema(schema)
+  }
+
+    return (
+        <div>
+        <h2>Namespace:  {props.namespace}</h2>
+        <h4>Schemas</h4>
+        {schemaList ? <SchemaList schemas={schemaList} selectSchema={selectSchema}/> : 
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+            }
+        
+        </div>
+    );
+}
+
+const SchemaList = (props) => {
+  const listItems = Object.values(props.schemas).map(schema => { 
+    return <li key={schema.schema}>
+      <a className="a" onClick={() => props.selectSchema(schema.schema)}>{schema.schema}</a> -- {schema.description}  <a href={schema.url}>Download</a>
+    </li>
+  })
+  console.log(props.schemas)
+  // const listItems = ["a", "b", "c"]
+
+    return (
+        <div>
+        <ul>
+        {listItems}
+        </ul>
+        </div>
+    );
+}
+
+
+const SchemaSelector = (props) => {
+    const [namespaceList, setNamespaceList] = useState([]);
+    useEffect(() => {
+      fetch('/namespaces.json')
+        .then(response => response.json())
+        .then(data => {
+          console.log("Namespaces:", data)
+          const options = data.map((item) => { 
+            return {"value": item, "label": item }
+          })
+          console.log("options:", options)
+          setNamespaceList(options)
+        })
+    }, [])
+
+    const selectNamespace = (selectedOption) => {
+        props.setSearchParams({ 'namespace': selectedOption.value })
+        props.setNamespace(selectedOption.value)
+
+    }
+    return (
+      <>
+        <h2>Select namespace</h2>
+        <Select options={namespaceList} onChange={selectNamespace} />
+      </>
+    );
+}
+
+export default App
